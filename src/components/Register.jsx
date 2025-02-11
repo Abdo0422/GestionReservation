8dd02171cd50c91cbbbf1054316781291078b17a
Abderrahmane Loubachi <119792655+Abdo0422@users.noreply.github.com>
2025-02-11 20:22:20 +0100

    Update Register.jsx

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,17 +1,24 @@
-import React, { useState } from 'react';
-import { TextField, Button, Typography, Box, Modal, Paper, Container  } from '@mui/material';
-import axios from '../features/axios';
-import { useDispatch } from 'react-redux';
-import { login } from '../features/actions';
-import { useNavigate } from 'react-router-dom';
-import { styled } from '@mui/system';
-
+import React, { useState } from "react";
+import {
+  TextField,
+  Button,
+  Typography,
+  Box,
+  Modal,
+  Paper,
+  Container,
+} from "@mui/material";
+import axios from "../features/axios";
+import { useDispatch } from "react-redux";
+import { login } from "../features/actions";
+import { useNavigate } from "react-router-dom";
+import { styled } from "@mui/system";
 
 const StyledPaper = styled(Paper)(({ theme }) => ({
-  padding: '20px 30px',
-  borderRadius: '10px',
-  boxShadow: '0px 4px 20px rgba(0, 0, 0, 0.1)',
-  backgroundColor: '#fff',
+  padding: "2rem 3rem",
+  borderRadius: "10px",
+  boxShadow: "0px 4px 20px rgba(0, 0, 0, 0.1)",
+  backgroundColor: "#fff",
 }));
 
 const StyledTextField = styled(TextField)(({ theme }) => ({
@@ -20,52 +27,60 @@ const StyledTextField = styled(TextField)(({ theme }) => ({
 
 const StyledButton = styled(Button)(({ theme }) => ({
   marginBottom: theme.spacing(2),
-  backgroundColor: '#059669',
-  color: '#fff',
-  '&:hover': {
-    backgroundColor: '#10B981',
+  backgroundColor: "#059669",
+  color: "#fff",
+  "&:hover": {
+    backgroundColor: "#10B981",
   },
 }));
 
 const StyledModal = styled(Modal)(({ theme }) => ({
-  display: 'flex',
-  justifyContent: 'center',
-  alignItems: 'center',
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
 }));
 
 const ModalBox = styled(Box)(({ theme }) => ({
-  padding: 3,
-  backgroundColor: 'white',
-  borderRadius: 2,
-  boxShadow: 3,
-  width: '400px',
-  marginTop: '20vh',
+  padding: "2rem",
+  backgroundColor: "white",
+  borderRadius: "10px",
+  boxShadow: "0px 4px 20px rgba(0, 0, 0, 0.1)",
+  width: "400px",
+  maxWidth: "90vw",
+  marginTop: "10vh",
 }));
 
 const Register = () => {
-  const [username, setUsername] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState('');
-  const [verificationCode, setVerificationCode] = useState('');
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [verificationCode, setVerificationCode] = useState("");
   const [openModal, setOpenModal] = useState(false);
   const [error, setError] = useState(null);
-  const [verificationMessage, setVerificationMessage] = useState('');
+  const [verificationMessage, setVerificationMessage] = useState("");
   const dispatch = useDispatch();
   const navigate = useNavigate();
+  const [isVerifying, setIsVerifying] = useState(false);
 
   const handleSubmit = async (event) => {
     event.preventDefault();
 
     if (password !== confirmPassword) {
-      setError('Les mots de passe ne correspondent pas');
+      setError("Les mots de passe ne correspondent pas");
       return;
     }
 
     try {
-      const response = await axios.post('/register', { username, email, password });
-      console.log('Réponse:', response); 
-      setVerificationMessage('Un code de vérification a été envoyé à votre email. Veuillez le vérifier.');
+      const response = await axios.post("/register", {
+        username,
+        email,
+        password,
+      });
+      console.log("Réponse:", response);
+      setVerificationMessage(
+        "Un code de vérification a été envoyé à votre email. Veuillez le vérifier."
+      );
       setOpenModal(true);
 
       dispatch(
@@ -76,42 +91,67 @@ const Register = () => {
         })
       );
     } catch (error) {
-      console.error(error); 
-      setError(error.response ? error.response.data.error : 'Une erreur inattendue est survenue');
+      console.error(error);
+      setError(
+        error.response
+          ? error.response.data.error
+          : "Une erreur inattendue est survenue"
+      );
     }
   };
 
   const handleVerifyCode = async () => {
+    setIsVerifying(true);
+    setError(null);
     try {
-      const response = await axios.post('/verify', { email, code: verificationCode });
+      const response = await axios.post("/verify", {
+        email,
+        code: verificationCode,
+      });
+
       if (response && response.data) {
-        setVerificationMessage('Votre email a été vérifié avec succès!');
-        const loginResponse = await axios.post('/login', { email, password });
-        
+        setVerificationMessage("Votre email a été vérifié avec succès!");
+        const loginResponse = await axios.post("/login", { email, password });
+
         if (loginResponse && loginResponse.data) {
-          dispatch(login({
-            user: loginResponse.data,
-            role: loginResponse.data.role,
-            name: loginResponse.data.name,
-            numdep: loginResponse.data.numdep,
-          }));
-          navigate('/');
+          dispatch(
+            login({
+              user: loginResponse.data,
+              role: loginResponse.data.role,
+              name: loginResponse.data.name,
+              numdep: loginResponse.data.numdep,
+            })
+          );
+          navigate("/");
+          setOpenModal(false);
         } else {
-          setError('Échec de la connexion: Pas de données retournées du serveur');
+          setError(
+            "Échec de la connexion: Pas de données retournées du serveur"
+          );
         }
       } else {
-        setError('Échec de la vérification: Pas de données retournées du serveur');
+        setError(
+          "Échec de la vérification: Pas de données retournées du serveur"
+        );
       }
     } catch (error) {
       console.error(error);
-      setError(error.response ? error.response.data.error : 'Une erreur inattendue est survenue');
+      setError(
+        error.response
+          ? error.response.data.error
+          : "Une erreur inattendue est survenue"
+      );
+    } finally {
+      setIsVerifying(false);
     }
   };
 
   return (
     <Container maxWidth="sm">
       <StyledPaper>
-        <Typography variant="h4" align="center" sx={{ mb: 3, fontWeight: 600 }}>Inscription</Typography>
+        <Typography variant="h4" align="center" sx={{ mb: 3, fontWeight: 600 }}>
+          Inscription
+        </Typography>
         <form onSubmit={handleSubmit}>
           <StyledTextField
             fullWidth
@@ -141,7 +181,9 @@ const Register = () => {
             value={confirmPassword}
             onChange={(event) => setConfirmPassword(event.target.value)}
           />
-          <StyledButton fullWidth type="submit">S'inscrire</StyledButton>
+          <StyledButton fullWidth type="submit">
+            S'inscrire
+          </StyledButton>
           {error && (
             <Typography color="error" variant="body2">
               {error}
@@ -149,14 +191,16 @@ const Register = () => {
           )}
         </form>
         {verificationMessage && (
-          <Typography variant="body2" sx={{ color: 'green', textAlign: 'center', mb: 2 }}>
+          <Typography
+            variant="body2"
+            sx={{ color: "green", textAlign: "center", mb: 2 }}
+          >
             {verificationMessage}
           </Typography>
         )}
       </StyledPaper>
 
-      {/* Modal for Verification */}
-      <StyledModal open={openModal} onClose={() => setOpenModal(false)}>
+      <StyledModal open={openModal}>
         <ModalBox>
           <Typography variant="h6" sx={{ mb: 2 }}>
             Entrez le code de vérification
@@ -168,9 +212,15 @@ const Register = () => {
             value={verificationCode}
             onChange={(event) => setVerificationCode(event.target.value)}
           />
-          <StyledButton fullWidth onClick={handleVerifyCode}>Vérifier</StyledButton>
+          <StyledButton
+            fullWidth
+            onClick={handleVerifyCode}
+            disabled={isVerifying}
+          >
+            {isVerifying ? "Vérification en cours..." : "Vérifier"}
+          </StyledButton>
           {error && (
-            <Typography color="error" variant="body2" sx={{ mt: 2 }}>
+            <Typography color="error" variant="body2" sx={{ mt: 1 }}>
               {error}
             </Typography>
           )}
